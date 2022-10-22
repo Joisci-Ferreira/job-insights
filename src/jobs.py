c61f215ed406a8f0f7e7b3d1fd2436c6bb29fd7b
Joiscilene <joisci.lene@hotmail.com>
2022-10-22 18:53:01 -0300

    Adicionando a função read

--- src/jobs.py
+++ src/jobs.py
@@ -1,6 +1,7 @@
+import csv
 from functools import lru_cache
 
-##iniciando
+
 @lru_cache
 def read(path):
     """Reads a file from a given path and returns its contents
@@ -15,4 +16,10 @@ def read(path):
     list
         List of rows as dicts
     """
-    return []
+
+    try:
+        with open(path, encoding="utf8") as file:
+            jobs = csv.DictReader(file)
+            return [job for job in jobs]
+    except OSError:
+        print("File not found")
